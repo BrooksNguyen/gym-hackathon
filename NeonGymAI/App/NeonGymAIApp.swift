5ff9ede3cbd409a5a4121b4382a8c52806772243
Nguyễn Phúc Bách <phucbach1212@gmail.com>
2026-08-29 13:31:25 +0700

    Remove Onboarding flow

--- NeonGymAI/App/NeonGymAIApp.swift
+++ NeonGymAI/App/NeonGymAIApp.swift
@@ -3,17 +3,11 @@ import SwiftUI
 @main
 struct NeonGymAIApp: App {
     @AppStorage("isDarkMode") private var isDarkMode = true
-    @AppStorage("hasCompletedOnboarding") private var hasCompletedOnboarding = false
     
     var body: some Scene {
         WindowGroup {
-            if hasCompletedOnboarding {
-                MainTabView()
-                    .preferredColorScheme(isDarkMode ? .dark : .light)
-            } else {
-                OnboardingView()
-                    .preferredColorScheme(isDarkMode ? .dark : .light)
-            }
+            MainTabView()
+                .preferredColorScheme(isDarkMode ? .dark : .light)
         }
     }
 }
